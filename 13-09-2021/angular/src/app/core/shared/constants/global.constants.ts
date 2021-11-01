ae3586c374dc9e9b39c36cca5a16580758c26548
sittimani <67500879+sittimani@users.noreply.github.com>
2021-11-01 10:02:49 +0530

    Delete 13-09-2021 directory

--- 13-09-2021/angular/src/app/core/shared/constants/global.constants.ts
+++
@@ -1,4 +0,0 @@
-export enum Constants {
-    invalidForm = "Please Fill the form Correctly",
-    invalidFile = "Please Use a png or jpg file!!!"
-}
--- 13-09-2021/node/routes/register-route.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const registerController = require('../controllers/register-controller')
-
-router.post('/register', registerController.register)
-
-module.exports = router;

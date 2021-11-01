503fa9026b91a6642ffa0ac0288fbf547817569d
sittimani <67500879+sittimani@users.noreply.github.com>
2021-11-01 10:02:23 +0530

    Delete 08-09-2021 directory

--- 08-09-2021/node/controllers/register-controller.js
+++
@@ -1,8 +0,0 @@
-const registerService = require('../service/register.service')
-
-module.exports = {
-    register: async (request, response) => {
-        const res = await registerService.register(request.body)
-        response.json(res)
-    }
-}

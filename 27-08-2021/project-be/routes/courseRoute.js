11f4b6f63f1c40c55576aa44107f5007675a79c0
sittimani <67500879+sittimani@users.noreply.github.com>
2021-11-01 10:04:06 +0530

    Delete 27-08-2021 directory

--- 27-08-2021/project-be/routes/courseRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-//const router = express.Router;
-const courseController = require('../controllers/course-controller')
-
-router.get('/getAllCourse', courseController.getAllCourse)
-router.get('/getCourse/:name', courseController.getCourse)
-
-module.exports = router;

53ab5ea7090423d913944ab4a941608d3de16662
sittimani <67500879+sittimani@users.noreply.github.com>
2021-11-01 10:02:42 +0530

    Delete 09-09-2021 directory

--- 09-09-2021/angular/src/app/question-and-answer/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./shared/service/qa.service"
--- 09-09-2021/node/models/course-model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose')
-
-const Schema = mongoose.Schema
-const courseSchema = new Schema({}, {strict:false, timestamps: true, versionKey: false })
-
-
-module.exports = mongoose.model('courseSchema', courseSchema, "courses")

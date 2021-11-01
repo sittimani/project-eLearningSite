0111b94496ebe865d42dd3b4e4744f0de7335687
sittimani <67500879+sittimani@users.noreply.github.com>
2021-11-01 10:05:04 +0530

    Delete 31-08-2021 directory

--- 31-08-2021/be/routes/register-route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const registerController = require('../controllers/register-controller')
-
-router.post('/studentRegister', registerController.studentRegister)
-router.post('/professorRegister', registerController.professorRegister)
-
-
-module.exports = router;
